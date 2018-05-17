b196d079ec44fb1fc589e5f9c1fdf9b46cff6c08
pekerleke <koumsky@gmail.com>
2018-05-16 22:45:27 -0300

    Se agrego la conexion con la bd

--- core/core.php
+++ core/core.php
@@ -1,5 +1,8 @@
 <?php
-	
+	// seteo coonexion con la bd
+	$conexion=mysqli_connect("localhost","root","","base") or
+		die("Problemas con la conexión a la base de datos");
+
 	session_start();
 
 	$DB = array(
