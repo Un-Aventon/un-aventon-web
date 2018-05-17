13bda6a0039b71d4ceac51405496555e5e09bed5
pekerleke <koumsky@gmail.com>
2018-05-16 22:06:24 -0300

    estructura html basica y navbar

---
+++ php/inicio_body.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>UnAventon</title>
+
+    <!-- icon -->
+    <link rel='shortcut icon' type='image/png' href='img/logo.png' />
+    <!-- responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- CSS de bootstrap --> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- CSS principal --> <link rel="stylesheet" href="css/stylo.css">
+  </head>
+  <body>
+    <?php
+      include('php/navbar.php');
+    ?>
+    <div class="container" style="background-color: #fff">
---
+++ php/navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <img src="img/logo.png" alt="logo" style="width: 40px; margin-right: 8px">
+  <a class="navbar-brand" href="#">UnAventon</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Mi Perfil</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="A donde queres ir ?" aria-label="Search">
+      <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Buscar</button>
+    </form>
+  </div>
+</nav>
