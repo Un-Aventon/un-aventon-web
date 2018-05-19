d69ddb765788ee7e0cd26372efbff5dbed19be62
pekerleke <koumsky@gmail.com>
2018-05-19 15:20:23 -0300

    se agrego funcion saneadora de string

--- core/helpers.php
+++ core/helpers.php
@@ -17,10 +17,25 @@ function dias_transcurridos($fecha_alta)
  }
 }
 
-function calc_edad($fecha) { 
-    $tiempo = strtotime($fecha); 
-    $ahora = time(); 
-    $edad = ($ahora-$tiempo)/(60*60*24*365.25); 
-    $edad = floor($edad); 
-    return $edad; 
-} 
+function calc_edad($fecha) {
+    $tiempo = strtotime($fecha);
+    $ahora = time();
+    $edad = ($ahora-$tiempo)/(60*60*24*365.25);
+    $edad = floor($edad);
+    return $edad;
+}
+
+function comprobar_string($string){
+   if (strlen($string)<3 || strlen($string)>30){
+      return false;
+   }
+
+   //compruebo que los caracteres sean los permitidos
+   $permitidos = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_@.";
+   for ($i=0; $i<strlen($string); $i++){
+      if (strpos($permitidos, substr($string,$i,1))===false){
+         return false;
+      }
+   }
+   return true;
+}
