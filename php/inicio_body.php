3406d8f8c61650cc23b1d02aae23c5cb30b686e5
pekerleke <koumsky@gmail.com>
2018-05-19 11:29:41 -0300

    se arreglo inclusion de plantilla css

--- php/inicio_body.php
+++ php/inicio_body.php
@@ -8,11 +8,13 @@
     <link rel='shortcut icon' type='image/png' href='img/logo.png' />
     <!-- responsive -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- CSS de bootstrap --> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <!-- CSS principal --> <link rel="stylesheet" href="css/stylo.css">
+    <!-- CSS de bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- CSS principal -->
+    <link rel="stylesheet" href="/css/stylo.css">
   </head>
   <body>
     <?php
       include('php/navbar.php');
     ?>
-    <div class="container" style="background-color: #fff;">
+    <div class="container" style="background-color: #FFF; border-radius: 5px">
