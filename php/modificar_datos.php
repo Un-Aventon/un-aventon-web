fd5ee4603e61748b9aafb5901a956f8ef10a7e3d
pekerleke <koumsky@gmail.com>
2018-05-19 14:56:52 -0300

    cambio de ubicacion del script de modificacion de datos

--- php/modificar_datos.php
+++ php/modificar_datos.php
@@ -1,11 +1,4 @@
 <?php
-function render($vars = [])
-{
-  //###### agregar comprobacion de que no tenga viajes pendientes
-
-  // incluyo la conexion
-  include('php/conexion.php');
-
   mysqli_query($conexion,"UPDATE usuario set email='$_REQUEST[email]', nombre='$_REQUEST[nombre]', apellido='$_REQUEST[apellido]'
                           WHERE email = '$_SESSION[mail]'")
   or
@@ -15,13 +8,8 @@ function render($vars = [])
   $_SESSION['nombre']=$_REQUEST['nombre'];
   $_SESSION['apellido']=$_REQUEST['apellido'];
 
+  setcookie("modificacion_datos",true);
+
+  header('Location: /perfil');
+
   ?>
-  <div class="alert alert-success" role="alert" style="margin-top: 5px">
-    <a href="#" class="alert-link">Exito</a> se realizaron los cambios correctamente
-  </div>
-
-  <center> <a href="/perfil">volver a mi perfil</a> </center>
-  <?php
-
-
-}
