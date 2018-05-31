deed48a3ba6cd36182faeffcaca1639ca75fb140
pekerleke <koumsky@gmail.com>
2018-05-30 23:27:14 -0300

    se corrigio error en la comprobacion de mails rapetidos al momento de la modificacion

--- php/modificar_datos.php
+++ php/modificar_datos.php
@@ -4,17 +4,17 @@
                                  where idPiloto = $_SESSION[userId] and estado = 'activo'")
                                  or die("problemas en la busqueda de viajes activos del usuario".mysqli_error($conexion));
 
-                                 
+
     if (mysqli_num_rows($viajes_user) == 0){
 
         if ( (comprobar_string($_REQUEST['email'])) && (comprobar_string($_REQUEST['nombre'])) && (comprobar_string($_REQUEST['apellido'])) ){
 
           $usuarios=mysqli_query($conexion, "SELECT *
                                  from usuario
-                                 where email = '$_REQUEST[email]'")
+                                 where email = '$_REQUEST[email]' and IdUser <> '$_SESSION[userId]'")
                                  or die("problemas en la busqueda de usuarios con el mismo email".mysqli_error($conexion));
           if(mysqli_num_rows($usuarios) == 0){
-                                 
+
                 mysqli_query($conexion,"UPDATE usuario set email='$_REQUEST[email]', nombre='$_REQUEST[nombre]', apellido='$_REQUEST[apellido]'
                                         WHERE email = '$_SESSION[mail]'")
                                         or
