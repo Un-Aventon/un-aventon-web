0fc4f1a172305b30481df62ee1c3460d5849302c
pekerleke <koumsky@gmail.com>
2018-06-19 23:14:41 -0300

    se agrego scrool en el listado de postulaciones

--- controller/viaje.php
+++ controller/viaje.php
@@ -212,6 +212,7 @@
 				echo '<button type="button" class="btn btn-light btn-sm" style="margin-bottom: 10px; width: 100%">
   								Postulaciones/Participaciones totales <span class="badge badge-danger">'.mysqli_num_rows($participaciones_copiloto).'</span>
 							</button>';
+				echo '<div class="mCustomScrollbar" data-mcs-theme="dark-3" style="max-height: 300px; overflow: auto;">';
 				if(mysqli_num_rows($participaciones_copiloto)==0)
 						 {echo "<br><small>(no hay participaciones todavia)</small>";}
 				while ($participacion_copiloto=mysqli_fetch_array($participaciones_copiloto)){
@@ -249,7 +250,7 @@
 																	<input type="hidden" name="rechazar_postulacion" value="'.$vars[0].'">
 
 
-															<button type="submit" class="buttonText buttonTextRojo float-right">rechazar</button>
+															<button type="submit" class="buttonText buttonTextRojo float-right">rechazar </button>
 													</form></div>
 											</div>
 													</div>';
@@ -285,6 +286,7 @@
 						}
 						echo '</div>';
 				}
+				echo '</div>';
 			}
 			?>
 
