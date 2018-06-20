a12c8d647598fc4eca80ac5e1d32a8608899269a
FGasquez <federicogasquez@gmail.com>
2018-06-20 14:49:26 -0300

    Se agregaron las consultas sql del alta de viaje

--- php/alta_viaje.php
+++ php/alta_viaje.php
@@ -1,5 +1,6 @@
 <?php
 
+$ok = true;
 /** ORIGEN Y DESTINO **/
 $origen = [
 	'localidad' => $_POST['localidad_origen'],
@@ -10,6 +11,7 @@ $destino = [
 	'localidad' => $_POST['localidad_destino'],
 	'provincia' => $_POST['prov_destino']
 ];
+
 
 if(!($origen['localidad']) || !($origen['provincia']) || !($destino['localidad']) || !($destino['provincia']))
 {
@@ -41,10 +43,14 @@ $distance = json_decode($json_distance, true);
 //echo ($json_distance);
 
 
-$l = $distance['rows']['0']['elements']['0']['status'];
-
-if($l != 'OK' or ($distance['origin_addresses']['0'] == $distance['destination_addresses']['0'])){
-	echo "NO SE ENCOTRO RUTA";
+echo "<br/>";
+$l = $distance['rows']['1']['elements']['0']['status'];
+if($l != 'OK' or ($distance['origin_addresses']['0'] == $distance['destination_addresses']['0']))
+{
+	echo '<div class="alert alert-danger alert-dismissable">
+			<button type="button" class="close" data-dismiss="alert">&times;</button>
+				No se encontró ruta.
+		</div>';
 	$ok = false;
 }
 
@@ -52,8 +58,11 @@ if($l != 'OK' or ($distance['origin_addresses']['0'] == $distance['destination_a
 
 $fecha_salida = $_POST['fecha_salida'];
 
-if(!(isset($fecha_salida)) or ($fecha_salida < date("Y-m-d h:m:s"))){
-	echo "FECHA INVALIDA";
+if(!(isset($fecha_salida)) or ($fecha_salida < date("Y-m-d"))){
+	echo '<div class="alert alert-danger alert-dismissable">
+			<button type="button" class="close" data-dismiss="alert">&times;</button>
+				La fecha ingresada no es válida.
+		</div>';
 	$ok = false;
 }
 
@@ -61,7 +70,11 @@ if(!(isset($fecha_salida)) or ($fecha_salida < date("Y-m-d h:m:s"))){
 $hora_salida = $_POST['hora_salida'];
 if(!isset($hora_salida) or (($fecha_salida == date("Y-m-d") and ($hora_salida <= date("H:m")))))
 {
-	echo "nope";
+	echo '<div class="alert alert-danger alert-dismissable">
+		<button type="button" class="close" data-dismiss="alert">&times;</button>
+			La hora de salida ingresada no es válida.
+	</div>';
+	$ok = false;
 }else
 {
 	$fecha_partida = date($fecha_salida . ' ' . $hora_salida . ':00');
@@ -86,36 +99,25 @@ if(isset($_POST['intervalo_rep'])){
 $tiempo_estimado = $_POST['tiempo_estimado'];
 if(!is_numeric($tiempo_estimado) or ($tiempo_estimado < 1))
 {
-	echo 'TIEMPO INGRESADO NO ES VALIDO';
+	echo '<div class="alert alert-danger alert-dismissable">
+		<button type="button" class="close" data-dismiss="alert">&times;</button>
+			El tiempo estimado ingresado no es válido.
+	</div>';
 	$ok = false;
 }
 
 /** VEHICULO SELECCIONADO **/
 $id_vehiculo = $_POST['vehiculo'];
-$vehiculo = mysqli_query($conexion, "
-	SELECT *
-	from vehiculo v, viaje v2 
-	where v.idVehiculo = '$id_vehiculo'
-	and v.idVehiculo = v2.idVehiculo
-	and(
-		('$fecha_partida' BETWEEN v2.fecha_partida AND DATE_ADD(v2.fecha_partida, INTERVAL v2.tiempo_estimado HOUR))
-	    or (DATE_ADD('$fecha_partida', INTERVAL $tiempo_estimado HOUR) BETWEEN v2.fecha_partida AND DATE_ADD(v2.fecha_partida, INTERVAL v2.tiempo_estimado HOUR))
-	    or (v2.fecha_partida BETWEEN 'fecha_partida' AND DATE_ADD('fecha_partida', INTERVAL v2.tiempo_estimado HOUR))
-	    or (DATE_ADD(v2.fecha_partida, INTERVAL v2.tiempo_estimado HOUR) BETWEEN 'fecha_partida' AND DATE_ADD('fecha_partida', INTERVAL v2.tiempo_estimado HOUR))
-	)
-");
-
-if($viaje_postulado = mysqli_fetch_array($vehiculo))
-{
-	echo "EL VEHICULO ESTA POSTULADO en el viaje de " . $viaje_postulado['origen'] . ' a ' . $viaje_postulado['destino'] . ' de la misma fecha';
-	$ok = false;
-}
 
 
 /** CANTIDAD DE ASIENTOS **/
 $cant_asientos = $_POST['asientos'];
-if(!$cant_asientos > 0){ 
-	echo "La cantidad de asientos debe ser mayor a 1";
+if(!is_numeric($cant_asientos) or ($cant_asientos < 1))
+{
+	echo '<div class="alert alert-danger alert-dismissable">
+			<button type="button" class="close" data-dismiss="alert">&times;</button>
+				La cantidad de asientos a postular debe ser mayor a 1.
+		</div>';
 	$ok = false;
 }
 
@@ -123,23 +125,105 @@ $verif_asientos = mysqli_query($conexion, "SELECT cant_asientos from vehiculo wh
 
 if($asientos_total = mysqli_fetch_array($verif_asientos) < $cant_asientos)
 {
-	echo "La cantidad de asientos a postular para este vehiculo debe ser menor a " . $asientos_total;
+	echo '<div class="alert alert-danger alert-dismissable">
+			<button type="button" class="close" data-dismiss="alert">&times;</button>
+				La cantidad de asientos a postular para este vehiculo debe ser menor a ' . $asientos_total .'
+		</div>';	
 	$ok = false;
 }
 
 /** COSTO DEL VIAJE**/
 
 $costo = $_POST['precio'];
-if(!is_numeric($costo) or $costo < 0)
+if(!is_numeric($costo) or $costo < 1)
 {
-	echo "PRECIO INVALIDO";
+	echo '<div class="alert alert-danger alert-dismissable">
+			<button type="button" class="close" data-dismiss="alert">&times;</button>
+				El precio ingresado es inválido.
+		</div>';
 	$ok = false;
 }
 
 if($ok)
 {
-	for ($i=0; $i < $cant_intervalos +1; $i++) { 
-		echo "al touch / ";
 
+	$piloto = $_SESSION['userId'];
+	$c_origen = $origen['localidad'] . ',' . $origen['provincia'];
+	$c_destino = $destino['localidad'] . ',' . $destino['provincia'];
+	$cargados = 0;
+	$errores = 0;
+	$fechas_cargadas = [];
+	$fechas_erroneas = [];
+	for ($i=0; $i < $cant_intervalos +1; $i++) 
+	{ 
+		//echo $fecha_partida;
+		if(es_fecha_valida($conexion, $id_vehiculo, $fecha_partida, $tiempo_estimado) < 1 )
+		{
+		
+			$carga = mysqli_query($conexion, "INSERT into viaje (idPiloto, idVehiculo, fecha_publicacion, fecha_partida, tiempo_estimado,  origen, destino, asientos_disponibles, costo) values ('$piloto', '$id_vehiculo', now(), '$fecha_partida', '$tiempo_estimado' ,'$c_origen' , '$c_destino', '$cant_asientos', '$costo') ") or die ('nope ' . mysqli_error($conexion));
+		
+
+			$last_id = mysqli_query($conexion, "SELECT MAX(idViaje) AS id FROM viaje") or die(mysqli_error($conexion));
+			$l_id = mysqli_fetch_array($last_id);
+
+			array_push($fechas_cargadas, array(
+				'fecha' => $fecha_partida,
+				'id' => $l_id['id']
+			));
+			$cargados++;
+		}
+		else
+		{
+			$errores++;
+			array_push($fechas_erroneas, $fecha_partida);
+		}
+
+		//sumo los dias del intervalo
+		$fecha_partida = date(sum_days($fecha_partida, '+'. $intervalo) . " " . $hora_salida . ':00');
+
+	}
+
+
+	if($cargados > 0)
+	{	
+		echo '	<div class="alert alert-success" role="alert">
+					<p>Los siguientes viajes fueron cargados correctamente.</p>
+					<hr>
+					<p class="mb-0">';
+		 			 	foreach ($fechas_cargadas as $key) {
+					 		echo '<a href="/viaje/' . $key['id'] . '">' . $key['fecha'] . '</a><br/>';
+					 	}
+		echo '		</p>
+				</div>';
+
+	}
+	
+
+		if($errores > 0)
+		{	
+			if($cargados > 1)
+			{
+				echo '	<div class="alert alert-danger" role="alert">
+							<p>Los siguientes viajes no pudieron ser cargados por que el vehiculo se encuentra postulado a otro viaje en esas fechas.</p>
+							<hr>
+							<p class="mb-0">';
+				 			 	foreach ($fechas_erroneas as $key) {
+							 		echo $key . '<br/>';
+							 	}
+				echo '		</p>
+							<a href="/altaviaje"> Cargar en otra fecha </a>
+						</div>';
+			}
+			else
+			{
+				echo '<div class="alert alert-danger alert-dismissable">
+						<button type="button" class="close" data-dismiss="alert">&times;</button>
+							El vehiculo se encuentra postulado en otro viaje para esas fechas.
+					</div>';
+			}
+		};
+	if($cargados > 0)
+	{
+		$form = false;
 	}
 }
