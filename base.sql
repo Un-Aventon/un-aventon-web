54175088dd79797433fef0baee8e025fdec74726
marianomartinelli <mariano_martinelli@yahoo.com.ar>
2018-07-13 14:27:44 -0300

    Base con idViaje en la tabla 'calificacion'

--- base.sql
+++ base.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 28-06-2018 a las 14:23:10
--- Versión del servidor: 5.7.21
--- Versión de PHP: 5.6.35
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-07-2018 a las 19:21:49
+-- Versión del servidor: 10.1.34-MariaDB
+-- Versión de PHP: 7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,45 +28,48 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `calificacion`
 --
 
-DROP TABLE IF EXISTS `calificacion`;
-CREATE TABLE IF NOT EXISTS `calificacion` (
-  `idCalificacion` int(6) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `calificacion` (
+  `idCalificacion` int(6) NOT NULL,
   `idCalificador` int(6) NOT NULL,
   `idCalificado` int(6) NOT NULL,
   `tipo` varchar(10) NOT NULL,
-  `idViaje` int(6) NOT NULL,	
+  `idViaje` int(6) NOT NULL,
   `fecha` datetime NOT NULL,
   `calificacion` varchar(6) DEFAULT NULL,
-  `comentario` varchar(255) NOT NULL,
-  PRIMARY KEY (`idCalificacion`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
+  `comentario` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `calificacion`
 --
 
-INSERT INTO `calificacion` (`idCalificacion`, `idCalificador`, `idCalificado`, `tipo`, `fecha`, `calificacion`, `comentario`) VALUES
-(18, 0, 2, '3', '2018-06-27 17:11:45', '-1', 'penalizacion por rechazo de postulacion'),
-(19, 0, 2, '3', '2018-06-27 17:16:39', '-1', 'penalizacion por rechazo de postulacion'),
-(20, 5, 1, '1', '2018-06-27 00:00:00', NULL, ''),
-(21, 0, 7, '3', '2018-06-27 22:34:39', '-1', 'penalizacion por rechazo de postulacion'),
-(22, 0, 7, '3', '2018-06-27 22:42:37', '-1', 'penalizacion por rechazo de postulacion'),
-(23, 0, 7, '3', '2018-06-27 22:42:37', '0', 'harcodeada'),
-(25, 0, 7, '3', '2018-06-27 22:42:37', '0', 'harcodeada'),
-(26, 0, 7, '3', '2018-06-27 22:42:37', '0', 'harcodeada'),
-(27, 0, 7, '3', '2018-06-27 22:42:37', '1', 'harcodeada'),
-(28, 0, 7, '3', '2018-06-27 22:42:37', '1', 'harcodeada'),
-(29, 0, 7, '3', '2018-06-27 22:42:37', '1', 'harcodeada'),
-(30, 0, 7, '3', '2018-06-27 22:42:37', '1', 'harcodeada'),
-(31, 0, 7, '3', '2018-06-27 22:42:37', '1', 'harcodeada'),
-(32, 0, 6, '3', '2018-06-27 22:42:37', '1', 'harcodeada'),
-(33, 0, 6, '3', '2018-06-27 22:42:37', '1', 'harcodeada'),
-(34, 0, 6, '3', '2018-06-27 22:42:37', '1', 'harcodeada'),
-(35, 0, 6, '3', '2018-06-27 22:42:37', '0', 'harcodeada'),
-(36, 0, 4, '3', '2018-06-27 22:42:37', '-1', 'harcodeada'),
-(37, 0, 4, '3', '2018-06-27 22:42:37', '-1', 'harcodeada'),
-(38, 0, 7, '3', '2018-06-28 11:02:12', '-1', 'penalizacion por rechazo de postulacion'),
-(39, 0, 7, '3', '2018-06-28 11:05:22', '-1', 'penalizacion por rechazo de postulacion');
+INSERT INTO `calificacion` (`idCalificacion`, `idCalificador`, `idCalificado`, `tipo`, `idViaje`, `fecha`, `calificacion`, `comentario`) VALUES
+(18, 0, 2, '3', 0, '2018-06-27 17:11:45', '-1', 'penalizacion por rechazo de postulacion'),
+(19, 0, 2, '3', 0, '2018-06-27 17:16:39', '-1', 'penalizacion por rechazo de postulacion'),
+(20, 5, 1, '1', 0, '2018-06-27 00:00:00', NULL, ''),
+(21, 0, 7, '3', 0, '2018-06-27 22:34:39', '-1', 'penalizacion por rechazo de postulacion'),
+(22, 0, 7, '3', 0, '2018-06-27 22:42:37', '-1', 'penalizacion por rechazo de postulacion'),
+(23, 0, 7, '3', 0, '2018-06-27 22:42:37', '0', 'harcodeada'),
+(25, 0, 7, '3', 0, '2018-06-27 22:42:37', '0', 'harcodeada'),
+(26, 0, 7, '3', 0, '2018-06-27 22:42:37', '0', 'harcodeada'),
+(27, 0, 7, '3', 0, '2018-06-27 22:42:37', '1', 'harcodeada'),
+(28, 0, 7, '3', 0, '2018-06-27 22:42:37', '1', 'harcodeada'),
+(29, 0, 7, '3', 0, '2018-06-27 22:42:37', '1', 'harcodeada'),
+(30, 0, 7, '3', 0, '2018-06-27 22:42:37', '1', 'harcodeada'),
+(31, 0, 7, '3', 0, '2018-06-27 22:42:37', '1', 'harcodeada'),
+(32, 0, 6, '3', 0, '2018-06-27 22:42:37', '1', 'harcodeada'),
+(33, 0, 6, '3', 0, '2018-06-27 22:42:37', '1', 'harcodeada'),
+(34, 0, 6, '3', 0, '2018-06-27 22:42:37', '1', 'harcodeada'),
+(35, 0, 6, '3', 0, '2018-06-27 22:42:37', '0', 'harcodeada'),
+(36, 0, 4, '3', 0, '2018-06-27 22:42:37', '-1', 'harcodeada'),
+(37, 0, 4, '3', 0, '2018-06-27 22:42:37', '-1', 'harcodeada'),
+(38, 0, 7, '3', 0, '2018-06-28 11:02:12', '-1', 'penalizacion por rechazo de postulacion'),
+(39, 0, 7, '3', 0, '2018-06-28 11:05:22', '1', 'hola mundo'),
+(40, 7, 5, '2', 47, '2018-07-11 07:20:00', '1', 'sss'),
+(41, 7, 5, '2', 48, '2018-07-11 07:20:00', '1', 'asdasdas'),
+(42, 7, 4, '2', 53, '2018-07-05 14:00:00', '1', 'buena onda los chabones'),
+(43, 7, 5, '2', 53, '2018-07-05 14:00:00', '1', 'buena onda los chabones'),
+(44, 7, 6, '2', 53, '2018-07-05 14:00:00', '1', 'buena onda los chabones');
 
 -- --------------------------------------------------------
 
@@ -74,13 +77,11 @@ INSERT INTO `calificacion` (`idCalificacion`, `idCalificador`, `idCalificado`, `
 -- Estructura de tabla para la tabla `estado_participacion`
 --
 
-DROP TABLE IF EXISTS `estado_participacion`;
-CREATE TABLE IF NOT EXISTS `estado_participacion` (
-  `idEstado` int(1) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `estado_participacion` (
+  `idEstado` int(1) NOT NULL,
   `color` varchar(14) NOT NULL,
-  `estado` varchar(40) NOT NULL,
-  PRIMARY KEY (`idEstado`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `estado` varchar(40) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `estado_participacion`
@@ -99,13 +100,11 @@ INSERT INTO `estado_participacion` (`idEstado`, `color`, `estado`) VALUES
 -- Estructura de tabla para la tabla `estado_viaje`
 --
 
-DROP TABLE IF EXISTS `estado_viaje`;
-CREATE TABLE IF NOT EXISTS `estado_viaje` (
-  `idEstado` int(1) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `estado_viaje` (
+  `idEstado` int(1) NOT NULL,
   `color` varchar(14) NOT NULL,
-  `estado` varchar(14) NOT NULL,
-  PRIMARY KEY (`idEstado`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `estado` varchar(14) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `estado_viaje`
@@ -122,14 +121,12 @@ INSERT INTO `estado_viaje` (`idEstado`, `color`, `estado`) VALUES
 -- Estructura de tabla para la tabla `pago`
 --
 
-DROP TABLE IF EXISTS `pago`;
-CREATE TABLE IF NOT EXISTS `pago` (
-  `idPago` int(6) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `pago` (
+  `idPago` int(6) NOT NULL,
   `idViaje` int(6) NOT NULL,
   `fecha` date NOT NULL,
-  `hora` time NOT NULL,
-  PRIMARY KEY (`idPago`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `hora` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `pago`
@@ -144,15 +141,13 @@ INSERT INTO `pago` (`idPago`, `idViaje`, `fecha`, `hora`) VALUES
 -- Estructura de tabla para la tabla `participacion`
 --
 
-DROP TABLE IF EXISTS `participacion`;
-CREATE TABLE IF NOT EXISTS `participacion` (
-  `idParticipacion` int(6) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `participacion` (
+  `idParticipacion` int(6) NOT NULL,
   `idUsuario` int(6) NOT NULL,
   `idViaje` int(6) NOT NULL,
   `fecha_solicitud` datetime NOT NULL,
-  `estado` int(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`idParticipacion`)
-) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=latin1;
+  `estado` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `participacion`
@@ -164,7 +159,10 @@ INSERT INTO `participacion` (`idParticipacion`, `idUsuario`, `idViaje`, `fecha_s
 (45, 6, 50, '2018-06-27 22:24:52', 1),
 (46, 4, 50, '2018-06-27 22:25:22', 1),
 (47, 6, 51, '2018-06-28 11:00:03', 1),
-(48, 6, 52, '2018-06-28 11:04:07', 2);
+(48, 6, 52, '2018-06-28 11:04:07', 2),
+(49, 4, 53, '2018-06-28 11:03:03', 5),
+(50, 5, 53, '2018-06-28 11:03:03', 5),
+(51, 6, 54, '2018-06-28 11:03:03', 5);
 
 -- --------------------------------------------------------
 
@@ -172,16 +170,14 @@ INSERT INTO `participacion` (`idParticipacion`, `idUsuario`, `idViaje`, `fecha_s
 -- Estructura de tabla para la tabla `pregunta`
 --
 
-DROP TABLE IF EXISTS `pregunta`;
-CREATE TABLE IF NOT EXISTS `pregunta` (
-  `idPregunta` int(6) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `pregunta` (
+  `idPregunta` int(6) NOT NULL,
   `idPreguntante` int(6) NOT NULL,
   `idViaje` int(6) NOT NULL,
   `pregunta` varchar(255) NOT NULL,
   `respuesta` varchar(255) NOT NULL,
-  `fecha` datetime NOT NULL,
-  PRIMARY KEY (`idPregunta`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `pregunta`
@@ -198,13 +194,11 @@ INSERT INTO `pregunta` (`idPregunta`, `idPreguntante`, `idViaje`, `pregunta`, `r
 -- Estructura de tabla para la tabla `tipo_vehiculo`
 --
 
-DROP TABLE IF EXISTS `tipo_vehiculo`;
-CREATE TABLE IF NOT EXISTS `tipo_vehiculo` (
-  `idTipo` int(1) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tipo_vehiculo` (
+  `idTipo` int(1) NOT NULL,
   `icono` varchar(140) DEFAULT NULL,
-  `tipo` varchar(11) NOT NULL,
-  PRIMARY KEY (`idTipo`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `tipo` varchar(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `tipo_vehiculo`
@@ -222,16 +216,14 @@ INSERT INTO `tipo_vehiculo` (`idTipo`, `icono`, `tipo`) VALUES
 -- Estructura de tabla para la tabla `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `idUser` int(6) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuario` (
+  `idUser` int(6) NOT NULL,
   `email` varchar(50) NOT NULL,
   `clave` varchar(50) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellido` varchar(50) NOT NULL,
-  `admin` int(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  `admin` int(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuario`
@@ -249,9 +241,8 @@ INSERT INTO `usuario` (`idUser`, `email`, `clave`, `nombre`, `apellido`, `admin`
 -- Estructura de tabla para la tabla `vehiculo`
 --
 
-DROP TABLE IF EXISTS `vehiculo`;
-CREATE TABLE IF NOT EXISTS `vehiculo` (
-  `idVehiculo` int(6) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `vehiculo` (
+  `idVehiculo` int(6) NOT NULL,
   `idPropietario` int(6) NOT NULL,
   `cant_asientos` int(2) NOT NULL,
   `modelo` varchar(30) NOT NULL,
@@ -259,9 +250,8 @@ CREATE TABLE IF NOT EXISTS `vehiculo` (
   `color` varchar(20) NOT NULL,
   `patente` varchar(10) NOT NULL COMMENT 'puede ser patente extrangera',
   `tipo` int(1) DEFAULT NULL,
-  `eliminado` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`idVehiculo`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+  `eliminado` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `vehiculo`
@@ -278,9 +268,8 @@ INSERT INTO `vehiculo` (`idVehiculo`, `idPropietario`, `cant_asientos`, `modelo`
 -- Estructura de tabla para la tabla `viaje`
 --
 
-DROP TABLE IF EXISTS `viaje`;
-CREATE TABLE IF NOT EXISTS `viaje` (
-  `idViaje` int(6) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `viaje` (
+  `idViaje` int(6) NOT NULL,
   `idPiloto` int(6) NOT NULL,
   `idVehiculo` int(6) NOT NULL,
   `fecha_publicacion` datetime NOT NULL,
@@ -291,9 +280,8 @@ CREATE TABLE IF NOT EXISTS `viaje` (
   `asientos_disponibles` int(2) NOT NULL,
   `costo` int(7) NOT NULL,
   `estado` int(1) NOT NULL DEFAULT '1',
-  `tipo` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT 'unico',
-  PRIMARY KEY (`idViaje`)
-) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
+  `tipo` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT 'unico'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `viaje`
@@ -301,11 +289,140 @@ CREATE TABLE IF NOT EXISTS `viaje` (
 
 INSERT INTO `viaje` (`idViaje`, `idPiloto`, `idVehiculo`, `fecha_publicacion`, `fecha_partida`, `tiempo_estimado`, `origen`, `destino`, `asientos_disponibles`, `costo`, `estado`, `tipo`) VALUES
 (47, 5, 13, '2018-06-27 17:29:18', '2018-06-27 17:31:00', 1, 'la plata,Buenos Aires', 'buenos aires,Buenos Aires', 3, 100, 3, 'unico'),
-(48, 5, 13, '2018-06-27 17:33:49', '2018-12-31 23:59:00', 1, 'la plata,Buenos Aires', 'buenos aires,Buenos Aires', 1, 100, 2, 'unico'),
+(48, 5, 13, '2018-06-27 17:33:49', '2018-12-31 23:59:00', 1, 'la plata,Buenos Aires', 'buenos aires,Buenos Aires', 1, 100, 3, 'unico'),
 (49, 7, 15, '2018-06-27 21:27:40', '2018-06-27 23:30:00', 1, 'la plata,Buenos Aires', 'buenos aires,Buenos Aires', 3, 200, 2, 'unico'),
 (50, 7, 15, '2018-06-27 22:05:04', '2018-06-30 13:00:00', 2, 'chascomus,Buenos Aires', 'la plata,Buenos Aires', 1, 500, 1, 'unico'),
 (51, 7, 15, '2018-06-27 22:05:04', '2018-07-07 13:00:00', 2, 'chascomus,Buenos Aires', 'la plata,Buenos Aires', 3, 500, 1, 'recurrente'),
-(52, 7, 15, '2018-06-28 11:03:03', '2018-07-05 14:00:00', 7, 'nequen,NeuquÃ©n', 'el bolson,RÃ­o Negro', 1, 1200, 1, 'unico');
+(52, 7, 15, '2018-06-28 11:03:03', '2018-07-05 14:00:00', 7, 'nequen,NeuquÃ©n', 'el bolson,RÃ­o Negro', 1, 1200, 1, 'unico'),
+(53, 7, 15, '2018-06-28 11:03:03', '2018-07-05 14:00:00', 1, 'Buenos aires, la Plata', 'Buenos aires, Palermo', 3, 3000, 3, 'unico');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  ADD PRIMARY KEY (`idCalificacion`);
+
+--
+-- Indices de la tabla `estado_participacion`
+--
+ALTER TABLE `estado_participacion`
+  ADD PRIMARY KEY (`idEstado`);
+
+--
+-- Indices de la tabla `estado_viaje`
+--
+ALTER TABLE `estado_viaje`
+  ADD PRIMARY KEY (`idEstado`);
+
+--
+-- Indices de la tabla `pago`
+--
+ALTER TABLE `pago`
+  ADD PRIMARY KEY (`idPago`);
+
+--
+-- Indices de la tabla `participacion`
+--
+ALTER TABLE `participacion`
+  ADD PRIMARY KEY (`idParticipacion`);
+
+--
+-- Indices de la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+  ADD PRIMARY KEY (`idPregunta`);
+
+--
+-- Indices de la tabla `tipo_vehiculo`
+--
+ALTER TABLE `tipo_vehiculo`
+  ADD PRIMARY KEY (`idTipo`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUser`);
+
+--
+-- Indices de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD PRIMARY KEY (`idVehiculo`);
+
+--
+-- Indices de la tabla `viaje`
+--
+ALTER TABLE `viaje`
+  ADD PRIMARY KEY (`idViaje`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  MODIFY `idCalificacion` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- AUTO_INCREMENT de la tabla `estado_participacion`
+--
+ALTER TABLE `estado_participacion`
+  MODIFY `idEstado` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `estado_viaje`
+--
+ALTER TABLE `estado_viaje`
+  MODIFY `idEstado` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `pago`
+--
+ALTER TABLE `pago`
+  MODIFY `idPago` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `participacion`
+--
+ALTER TABLE `participacion`
+  MODIFY `idParticipacion` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+
+--
+-- AUTO_INCREMENT de la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+  MODIFY `idPregunta` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_vehiculo`
+--
+ALTER TABLE `tipo_vehiculo`
+  MODIFY `idTipo` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUser` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  MODIFY `idVehiculo` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `viaje`
+--
+ALTER TABLE `viaje`
+  MODIFY `idViaje` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
